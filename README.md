fe198b6f5b9b6e1a01c6054c97cf008c68f25bd0
And-01 <naresh.k.objects@gmail.com>
2019-01-11 11:37:45 +0530

    Update

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # Flutter Local Notification
 
-Youtube : https://www.youtube.com/watch?v=n8-dGz1yNC8
+Step 1 : Add flutter_local_notifications dependency in your pubspec.yaml file.
+dependencies:
+  flutter:
+    sdk: flutter
+  flutter_local_notifications:
+  
+Step 2 : Add VIBRATE and RECEIVE_BOOT_COMPLETED permission to your AndroidManifest.xml [ Optional ]
 
-Mediium : https://medium.com/@nitishk72/flutter-local-notification-1e43a353877b
+    <uses-permission android:name="android.permission.VIBRATE" />
+    <uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED"/>
+    
+Step 3: Add image inside drawable directory for Notificaiton Icon [ Optional ]
+( flutter_app_name > android > app > src > res > drawable > app_icon.png )
+If you don’t add image for notification icon then you have to tell the app to use app icon, which is stored in ‘mipmap’. [ ANDROID ]
+
+
+Step 4 : Add a Tone (Music) for Notification [ Optional ]
+
+To add custom Tone (Music)
+
+Make a directory inside res directory.
+Keep a small Audio (Music) there and remember the file name.
+( flutter_app_name > android > app > src > res > raw > slow_spring_board.mp3 )
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
@@ -28,9 +29,9 @@ class _MyAppState extends State<MyApp> {
     showDialog(
       context: context,
       builder: (_) => new AlertDialog(
-        title: new Text('Notification'),
-        content: new Text('$payload'),
-      ),
+            title: new Text('Notification'),
+            content: new Text('$payload'),
+          ),
     );
   }
 
@@ -41,26 +42,116 @@ class _MyAppState extends State<MyApp> {
         title: new Text('Flutter Local Notification'),
       ),
       body: new Center(
-        child: new RaisedButton(
-          onPressed: showNotification,
-          child: new Text(
-            'Demo',
-            style: Theme.of(context).textTheme.headline,
-          ),
+        child: Column(
+          children: <Widget>[
+            new RaisedButton(
+              onPressed: showNotification,
+              child: new Text(
+                'Fire Notification',
+                style: Theme.of(context).textTheme.button,
+              ),
+            ),
+            new SizedBox(
+              height: 30.0,
+            ),
+            new RaisedButton(
+              onPressed: _showNotificationWithSound,
+              child: new Text(
+                'Show Notification With Sound',
+                style: Theme.of(context).textTheme.button,
+              ),
+            ),
+            new SizedBox(
+              height: 30.0,
+            ),
+            new RaisedButton(
+              onPressed: _showNotificationWithoutSound,
+              child: new Text(
+                'Show Notification Without Sound',
+                style: Theme.of(context).textTheme.button,
+              ),
+            ),
+            new SizedBox(
+              height: 30.0,
+            ),
+            new RaisedButton(
+              onPressed: _showNotificationWithDefaultSound,
+              child: new Text(
+                'Show Notification With Default Sound',
+                style: Theme.of(context).textTheme.button,
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 
+  // Method 1
+
   showNotification() async {
     var android = new AndroidNotificationDetails(
         'channel id', 'channel NAME', 'CHANNEL DESCRIPTION',
-        priority: Priority.High,importance: Importance.Max
-    );
+        priority: Priority.High, importance: Importance.Max);
     var iOS = new IOSNotificationDetails();
     var platform = new NotificationDetails(android, iOS);
     await flutterLocalNotificationsPlugin.show(
         0, 'New Video is out', 'Flutter Local Notification', platform,
-        payload: 'Nitish Kumar Singh is part time Youtuber');
+        payload: 'How to Show Notification in Flutter');
+  }
+
+  // Method 2 ==>  If you have skipped step 4 then Method 2 is not for you
+  Future _showNotificationWithSound() async {
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+        'your channel id', 'your channel name', 'your channel description',
+        sound: 'slow_spring_board',
+        importance: Importance.Max,
+        priority: Priority.High);
+    var iOSPlatformChannelSpecifics =
+        new IOSNotificationDetails(sound: "slow_spring_board.aiff");
+    var platformChannelSpecifics = new NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'New Post',
+      'How to Show Notification in Flutter',
+      platformChannelSpecifics,
+      payload: 'Custom_Sound',
+    );
+  }
+
+// Method 3
+  Future _showNotificationWithDefaultSound() async {
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+        'your channel id', 'your channel name', 'your channel description',
+        importance: Importance.Max, priority: Priority.High);
+    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+    var platformChannelSpecifics = new NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'New Post',
+      'How to Show Notification in Flutter',
+      platformChannelSpecifics,
+      payload: 'Default_Sound',
+    );
+  }
+
+// Method 4
+  Future _showNotificationWithoutSound() async {
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+        'your channel id', 'your channel name', 'your channel description',
+        playSound: false, importance: Importance.Max, priority: Priority.High);
+    var iOSPlatformChannelSpecifics =
+        new IOSNotificationDetails(presentSound: false);
+    var platformChannelSpecifics = new NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'New Post',
+      'How to Show Notification in Flutter',
+      platformChannelSpecifics,
+      payload: 'No_Sound',
+    );
   }
 }
